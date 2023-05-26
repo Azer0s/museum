732f0f8317ce437eaa2053ca6fb7444f092ae226
Azer0s <ariel@simulevski.at>
2023-05-26 18:05:16 +0200

    Add logging to health endpoint

--- http/health/routes.go
+++ http/health/routes.go
@@ -2,19 +2,24 @@ package health
 
 import (
 	_ "embed"
+	"go.uber.org/zap"
 	"museum/http/router"
 	"net/http"
 )
 
-func healthEndpoint(res *router.Response, _ *router.Request) {
-	res.WriteHeader(http.StatusOK)
-	err := router.WriteStatus(res, router.Status{Status: "OK"})
-	if err != nil {
-		//TODO: log error
-		return
+func healthEndpoint(log *zap.SugaredLogger) func(res *router.Response, req *router.Request) {
+	return func(res *router.Response, req *router.Request) {
+		log.Debug("handling health endpoint request", "requestId", req.RequestID)
+
+		res.WriteHeader(http.StatusOK)
+		err := router.WriteStatus(res, router.Status{Status: "OK"})
+		if err != nil {
+			log.Errorw("error writing status", "error", err, "requestId", req.RequestID)
+			return
+		}
 	}
 }
 
-func RegisterRoutes(r *router.Mux) {
-	r.AddRoute(router.Get("/api/health", healthEndpoint))
+func RegisterRoutes(r *router.Mux, log *zap.SugaredLogger) {
+	r.AddRoute(router.Get("/api/health", healthEndpoint(log)))
 }
