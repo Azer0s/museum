3a739c7edde209887a4da3eb7ff7117336f385ab
Ariel Simulevski <ariel@simulevski.at>
2023-08-19 23:36:41 +0200

    Refactor dispatch to cloudevents

--- persistence/impl/nats_eventing.go
+++ persistence/impl/nats_eventing.go
@@ -2,6 +2,8 @@ package impl
 
 import (
 	"context"
+	cloudevents "github.com/cloudevents/sdk-go/v2"
+	"github.com/google/uuid"
 	"github.com/nats-io/nats.go"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/trace"
@@ -26,7 +28,25 @@ func (n NatsEventing) DispatchExhibitCreatedEvent(ctx context.Context, exhibit d
 	n.Log.Debugw("nats eventing dispatching exhibit created event", "exhibitId", exhibit.Id)
 	span.AddEvent("dispatching exhibit created event")
 
-	err := n.Conn.Publish(n.Config.GetNatsBaseKey()+".exhibit.created", []byte(exhibit.Id))
+	event := cloudevents.NewEvent()
+	event.SetID(uuid.New().String())
+	event.SetSource("museum")
+	event.SetType("exhibit.created")
+	err := event.SetData(cloudevents.ApplicationJSON, map[string]string{"exhibitId": exhibit.Id})
+	if err != nil {
+		n.Log.Errorw("error setting event data", "error", err)
+		span.RecordError(err)
+		return
+	}
+
+	bytes, err := event.MarshalJSON()
+	if err != nil {
+		n.Log.Errorw("error marshalling event", "error", err)
+		span.RecordError(err)
+		return
+	}
+
+	err = n.Conn.Publish(n.Config.GetNatsBaseKey()+".exhibit.created", bytes)
 	if err != nil {
 		n.Log.Errorw("error publishing exhibit created event", "error", err)
 		span.RecordError(err)
