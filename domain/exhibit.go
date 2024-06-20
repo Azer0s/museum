bbef5f874a31610a554983b3793ce498300f1025
Azer0s <ariel@simulevski.at>
2024-06-20 18:55:36 +0200

    Add metadata

--- domain/exhibit.go
+++ domain/exhibit.go
@@ -1,14 +1,15 @@
 package domain
 
 type Exhibit struct {
-	Id          string              `json:"id"`
-	Name        string              `json:"name" yaml:"name"`
-	Expose      string              `json:"expose" yaml:"expose"`
-	Rewrite     *bool               `json:"rewrite" yaml:"rewrite"`
-	Objects     []Object            `json:"objects" yaml:"objects"`
-	Lease       string              `json:"lease" yaml:"lease"`
-	Order       []string            `json:"order" yaml:"order"`
-	RuntimeInfo *ExhibitRuntimeInfo `json:"-"`
+	Id          string                 `json:"id"`
+	Name        string                 `json:"name" yaml:"name"`
+	Expose      string                 `json:"expose" yaml:"expose"`
+	Rewrite     *bool                  `json:"rewrite" yaml:"rewrite"`
+	Objects     []Object               `json:"objects" yaml:"objects"`
+	Lease       string                 `json:"lease" yaml:"lease"`
+	Order       []string               `json:"order" yaml:"order"`
+	Meta        map[string]interface{} `json:"meta" yaml:"meta"`
+	RuntimeInfo *ExhibitRuntimeInfo    `json:"-"`
 }
 
 func (e Exhibit) ToDto() ExhibitDto {
@@ -23,6 +24,7 @@ func (e Exhibit) ToDto() ExhibitDto {
 		RuntimeInfo: e.RuntimeInfo.ToDto(),
 		Lease:       e.Lease,
 		Objects:     objects,
+		Meta:        e.Meta,
 	}
 }
 
--- domain/exhibit_dto.go
+++ domain/exhibit_dto.go
@@ -1,16 +1,19 @@
 package domain
 
 type ExhibitDto struct {
-	Id          string         `json:"id"`
-	Name        string         `json:"name"`
-	RuntimeInfo RuntimeInfoDto `json:"runtime_info"`
-	Lease       string         `json:"lease"`
-	Objects     []ObjectDto    `json:"objects"`
+	Id          string                 `json:"id"`
+	Name        string                 `json:"name"`
+	RuntimeInfo RuntimeInfoDto         `json:"runtime_info"`
+	Lease       string                 `json:"lease"`
+	Objects     []ObjectDto            `json:"objects"`
+	Meta        map[string]interface{} `json:"meta"`
 }
 
 func (d ExhibitDto) ToExhibit() Exhibit {
 	return Exhibit{
-		Id:   d.Id,
-		Name: d.Name,
+		Id:    d.Id,
+		Name:  d.Name,
+		Lease: d.Lease,
+		Meta:  d.Meta,
 	}
 }
